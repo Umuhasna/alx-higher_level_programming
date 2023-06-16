eecc8ff851e4e2067acc8a45d2701d56479795f6
Umuhasna <umuhasna123@gmail.com>
2023-06-17 00:14:04 +0300

    Fix it make it return thhe first key if the dictionary contains only one item

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,7 +4,7 @@ def best_score(a_dictionary):
     if a_dictionary:
         max_value = list(a_dictionary.values())[0]
         for key, value in a_dictionary.items():
-            if value > max_value:
+            if value >= max_value:
                 max_key = key
                 max_value = value
 
