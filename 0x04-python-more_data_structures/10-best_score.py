44594c342a2817f2cf0a9d3d17de85b54950e25f
Umuhasna <umuhasna123@gmail.com>
2023-06-17 00:05:22 +0300

    Fix make it return None if the dictionary is None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    max_key = None
     if a_dictionary:
-        max_key = None
         max_value = list(a_dictionary.values())[0]
         for key, value in a_dictionary.items():
             if value > max_value:
