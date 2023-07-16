40d079c8100042f06039533aa13bc06895739be8
Umuhasna <umuhasna123@gmail.com>
2023-07-17 01:10:56 +0300

    Fix improve the code so that it uses only 2 print as stated in the requirement

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,7 +2,7 @@
 def uppercase(str):
     for char in str:
         if (ord(char) >= 97 and ord(char) <= 122):
-            print("{:c}".format(ord(char) - 32), end="")
-        else:
-            print("{}".format(char), end="")
+            char = ord(char) - 32
+            char = chr(char)
+        print("{}".format(char), end="")
     print()
