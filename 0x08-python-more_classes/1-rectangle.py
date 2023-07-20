a4a366803be25d77ceef6839f40e0bb72d3df7a7
Umuhasna <umuhasna123@gmail.com>
2023-07-20 05:47:52 +0300

    Fix add the exceptions in the __init__ module

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,6 +19,15 @@ class Rectangle:
             width (int): width of the rectangle
             height (int): height of the rectangle
         """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
