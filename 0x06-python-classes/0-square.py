e70bd23c25ebd998ec7f55c2924b16813141e9f4
Umuhasna <umuhasna123@gmail.com>
2023-07-20 03:14:28 +0300

    Fix Add documentation for the module

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+'''
+This is a module that provide an empty class
+
+Classes:
+    Square: An empty class
+'''
+
+
 class Square:
+
     '''
     This is an empty class Square
 
