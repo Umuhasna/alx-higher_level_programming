dc4b9433a675409442b9a783076c87bc5a0f6c70
Umuhasna <umuhasna123@gmail.com>
2023-09-11 21:59:43 +0300

    Fix semiserror

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const size = parseInt(process.argv[2]);
 if (size) {
-  square = '';
+  let square = '';
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       square += 'X';
     }
     if (i !== size - 1) {
-    square += '\n';
+      square += '\n';
     }
   }
   console.log(square);
