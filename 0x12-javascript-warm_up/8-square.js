0e518cdefda823a0184557f9ae0ead595feb58f0
Umuhasna <umuhasna123@gmail.com>
2023-09-11 21:57:25 +0300

    Write a script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (size) {
+  square = '';
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      square += 'X';
+    }
+    if (i !== size - 1) {
+    square += '\n';
+    }
+  }
+  console.log(square);
+} else {
+  console.log('Missing size');
+}
