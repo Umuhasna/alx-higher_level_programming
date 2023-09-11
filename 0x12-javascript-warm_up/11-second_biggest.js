2fff0f0f3ffa294ea653fa2fe0c8373bf554d579
Umuhasna <umuhasna123@gmail.com>
2023-09-11 22:55:52 +0300

    Update convert the arguments to integer

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,9 +5,9 @@ if (process.argv.length <= 3) {
   const arr = process.argv.slice(2);
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < (arr.length - i - 1); j++) {
-      if (arr[j] > arr[j + 1]) {
-        const tmp = arr[j];
-        arr[j] = arr[j + 1];
+      if (parseInt(arr[j]) > parseInt(arr[j + 1])) {
+        const tmp = parseInt(arr[j]);
+        arr[j] = parseInt(arr[j + 1]);
         arr[j + 1] = tmp;
       }
     }
