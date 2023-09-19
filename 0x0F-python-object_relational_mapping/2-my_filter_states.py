1f4b6ac543a64d6faad5e760fcb102aa3c19e13e
Umuhasna04 <fa1033170@gmail.com>
2023-09-19 01:55:07 -0700

    Fix use format to create the SQL query with the user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
             )
     cur = conn.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-            (sys.argv[4],)
+            "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+            .format(sys.argv[4])
             )
     query_rows = cur.fetchall()
     for row in query_rows:
