69de26a8f6857af55570444093864ac735e69670
Umuhasna04 <fa1033170@gmail.com>
2023-09-19 01:59:56 -0700

    Fix make it case sensitive

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
             )
     cur = conn.cursor()
     cur.execute(
-            "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+            "SELECT * FROM states WHERE name LIKE binary '{}' ORDER BY id ASC"
             .format(sys.argv[4])
             )
     query_rows = cur.fetchall()
